9dc63f6d1724b58968e8c704d77d10be4e7da8e7
malekinho8 <shmeek8@gmail.com>
2023-06-20 18:21:03 -0400

    Edited visualization functions a bit as well as updates to example notebook

--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objs as go
+from mpl_toolkits.mplot3d import Axes3D
+
+# use latex for rendering in matplotlib
+plt.rc('text', usetex=True)
 
 def run(laser_azimuth, laser_polar, mirror_roll, mirror_pitch, mirror_yaw):
     incident_vector = calculate_incident_vector(laser_azimuth, laser_polar)
     plane_normal = calculate_plane_normal(mirror_roll, mirror_pitch, mirror_yaw)
     reflected_vector = calculate_reflected_vector(incident_vector, plane_normal)
     azimuth, polar = convert_to_spherical_coordinates(reflected_vector)
-    visualize_beam_and_surface(incident_vector, reflected_vector)
+    visualize_beam_and_surface_with_plotly(incident_vector, reflected_vector)
     print(f'Reflected Vector Azimuth: {azimuth} degrees')
     print(f'Reflected Vector Polar: {polar} degrees')
+    print(f'Reflected Vector Cartesian: {reflected_vector}')
 
 def calculate_incident_vector(azimuth, polar):
     """
@@ -126,6 +132,49 @@ def convert_to_spherical_coordinates(reflected_vector):
 
 def visualize_beam_and_surface(incident_vector, reflected_vector):
     """
+    Function to generate a 3D matplotlib visualization of the incident and reflected beams and the reflecting surface.
+
+    Parameters
+    ----------
+    incident_vector : numpy.ndarray
+        Direction vector of the incident beam in Cartesian coordinates (x, y, z).
+    reflected_vector : numpy.ndarray
+        Direction vector of the reflected beam in Cartesian coordinates (x, y, z).
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Add the incident beam to the figure
+    ax.plot([-incident_vector[0],0], [-incident_vector[1],0], [-incident_vector[2],0], color='blue', label='Incident Beam')
+
+    # Add the reflected beam to the figure
+    ax.plot([0,reflected_vector[0]], [0,reflected_vector[1]], [0,reflected_vector[2]], color='red', label='Reflected Beam')
+
+    # Set the axis labels and title with LaTeX
+    ax.set_xlabel('$X$')
+    ax.set_ylabel('$Y$')
+    ax.set_zlabel('$Z$')
+
+    ax.legend()
+
+    # Determine maximum absolute value across all vector components
+    max_value = np.max(np.abs([incident_vector, reflected_vector]))
+
+    # Set symmetric axis limits
+    ax.set_xlim([-max_value, max_value])
+    ax.set_ylim([-max_value, max_value])
+    ax.set_zlim([-max_value, max_value])
+
+    plt.title(r'Incident and Reflected Beams')
+
+    # Make the plot a "tight fit"
+    # plt.tight_layout()
+
+    # Show the figure
+    plt.show()
+
+def visualize_beam_and_surface_with_plotly(incident_vector, reflected_vector):
+    """
     Function to generate a 3D plotly visualization of the incident and reflected beams and the reflecting surface.
 
     Parameters
@@ -140,9 +189,9 @@ def visualize_beam_and_surface(incident_vector, reflected_vector):
 
     # add the incident beam to the figure
     fig.add_trace(go.Scatter3d(
-        x=[0, incident_vector[0]], # 0 indicates the starting value
-        y=[0, incident_vector[1]],
-        z=[0, incident_vector[2]],
+        x=[-incident_vector[0],0], # 0 indicates the starting value
+        y=[-incident_vector[1],0],
+        z=[-incident_vector[2],0],
         name='Incident Beam',
         mode='lines',
         line=dict(
@@ -164,12 +213,12 @@ def visualize_beam_and_surface(incident_vector, reflected_vector):
         )
     ))
 
-    # set the axes labels and title with LaTeX
+    # set the axes labels and title
     fig.update_layout(
         scene=dict(
-            xaxis_title='$X$',
-            yaxis_title='$Y$',
-            zaxis_title='$Z$',
+            xaxis_title='X',
+            yaxis_title='Y',
+            zaxis_title='Z',
             aspectmode='cube',
             camera=dict(
                 eye=dict(
@@ -179,8 +228,7 @@ def visualize_beam_and_surface(incident_vector, reflected_vector):
                 )
             )
         ),
-        title_text=r'$\text{Incident and Reflected Beams}$',
-        autosize=True, # this makes the plot fit the available space
+        title='Incident and Reflected Beams'
     )
 
     # show the figure
